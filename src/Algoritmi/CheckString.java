b38ba803e46ad7b0c8aa9db755c06214bb934312
hasan.saeed <has.23sad@gmail.com>
2024-04-24 17:37:01 +0200

    aggiunto metodo getToken

--- src/Algoritmi/CheckString.java
+++ src/Algoritmi/CheckString.java
@@ -2,6 +2,8 @@ package Algoritmi;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public abstract class CheckString {
 
@@ -16,6 +18,10 @@ public abstract class CheckString {
         standards.add("The islands of Philippine");
 
         System.out.println("Provo con l'algoritmo " + this.getClass().getSimpleName());
+        
+        if(input.contains(" ")) {
+        	
+        }
 
         for(String standard : standards){
             if(check(input,standard)){
@@ -45,6 +51,38 @@ public abstract class CheckString {
     }
     // TEMPLATE
     protected abstract boolean check(String input, String standard);
+    
+    
+    
+    // metodo Tokenizer
+    public List<String> getTokens(String str) {
+        List<String> tokens = new ArrayList<>();
+        
+        // ArrayList che contine le parole che non verranno analizzate nell'algoritmo
+        ArrayList<String> paroleEliminate=new ArrayList();
+        
+        // Qua è possibile inserire le le parole da eliminare dalla Stringa con Tokenizer
+        paroleEliminate.add("the");
+        paroleEliminate.add("of");
+        paroleEliminate.add("a");
+        paroleEliminate.add("an");
+        
+        StringTokenizer tokenizer = new StringTokenizer(str, " ");
+        while (tokenizer.hasMoreElements()) {
+            tokens.add(tokenizer.nextToken());
+        }
+        // Elimino dalla Stringa le parole che non servono
+        for(String parola:tokens) {
+        	for(String parolaNonVoluta:paroleEliminate) {
+        		if(parola.equalsIgnoreCase(parolaNonVoluta)) {
+        			tokens.remove(parolaNonVoluta);
+        		}
+        	}
+        }
+        return tokens;
+    }
+    
+    
 
 
 
